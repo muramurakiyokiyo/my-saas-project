28bcf736776e7cd5b911c5b716e0d7c2094c4db5
muramurakiyokiyo <kiyoichi.murata@gmail.com>
2025-12-29 07:36:57 +0900

    自動生成の準備

---
+++ orval.config.ts
@@ -0,0 +1,11 @@
+export default {
+  petstore: {
+    input: './petstore.yaml',
+    output: {
+      target: './app/generated/petstore.ts',
+      client: 'react-query', // ここで TanStack Query 用のコードを出すよう指定
+      mode: 'tags-split',
+    },
+  },
+};
+
