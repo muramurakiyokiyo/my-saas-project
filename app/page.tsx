7fe2afdf8066cfd716041305ee956709cad22fe7
muramurakiyokiyo <kiyoichi.murata@gmail.com>
2025-12-29 17:31:09 +0900

    なんかうまくいかんかったので元の状態に

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,11 @@
-import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query'
-import { getListPetsQueryOptions } from './generated/pets/pets'
 import PetForm from './components/PetForm'
 import PetList from './components/PetList'
 
-export default async function PetPage() {
-  const queryClient = new QueryClient()
-
-  // サーバー側でペット一覧データをプリフェッチ
-  // MSWがサーバーサイドでも動作するため、プリフェッチが成功する
-  await queryClient.prefetchQuery(getListPetsQueryOptions({ limit: 10 }))
-
+export default function PetPage() {
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <div className="p-8">
-        <PetForm />
-        <PetList />
-      </div>
-    </HydrationBoundary>
+    <div className="p-8">
+      <PetForm />
+      <PetList />
+    </div>
   )
 }
--- src/mocks/node.ts
+++
@@ -1,6 +0,0 @@
-import { setupServer } from 'msw/node'
-import { handlers } from './handlers'
-
-// サーバーサイド用のMSWサーバーをセットアップ
-export const server = setupServer(...handlers)
-
