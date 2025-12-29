2b052e640a34d5041b0f28d0c1ba030a81a30573
muramurakiyokiyo <kiyoichi.murata@gmail.com>
2025-12-29 17:24:48 +0900

    なんかの設定有効化→でもダメっぽいというか警告が増えた

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   experimental: {
     instrumentationHook: true,
-  },
+  } as any,
 };
 
 export default nextConfig;
